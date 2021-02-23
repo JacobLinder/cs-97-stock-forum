ef296f87c18b19968a550f44e82f25940c918f59
rahulmadnawat <59585630+rahulmadnawat@users.noreply.github.com>
2021-02-23 12:22:20 -0800

    Create Graph.js

---
+++ src/Graph.js
@@ -0,0 +1,23 @@
+import { csv } from 'd3';
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import { VictoryLine, VictoryChart } from 'Victory';
+
+const CSV = '/data/SPY.csv';
+
+const Graph = () => {
+  const [price, setPrice] = useState([]);
+  
+  useEffect(() => { 
+    csv(CSV, (ticker) => { return +ticker.Price; } )
+      .then(setPrice); }, []);
+  
+  return (
+    <VictoryChart width='1000' height='500'>
+    <VictoryLine data={price} x='Date' y='Price' />
+    </VictoryChart>
+  );
+};
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<Graph />, rootElement);
