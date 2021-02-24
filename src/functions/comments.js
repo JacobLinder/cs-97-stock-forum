7e90f7a2e830c17aea6ef518ea72ef47807ec662
JacobLinder <jacob.linder.jl@gmail.com>
2021-02-23 21:25:42 -0600

    comments

---
+++ src/functions/comments.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const db = firebase.firestore();
+
+// addComme
+export async function addReply(commentId) {
+  db
+}
