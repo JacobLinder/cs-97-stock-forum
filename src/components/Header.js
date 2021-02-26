fa9d9199ec4ccb3406710c234b7f5446515515e8
Nukidy <nuknuk@ucla.edu>
2021-02-25 17:02:12 -0800

    Initial landing page design

---
+++ src/components/Header.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { AppBar, IconButton, Toolbar, Collapse } from '@material-ui/core';
+import SortIcon from '@material-ui/icons/Sort';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+const useStyles = makeStyles((theme) => ({
+    root:{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      fontFamily: 'Nunito',
+    },
+  appbar:{
+    background: 'none',
+  },
+  appbarWrapper:{
+    width:"88%",
+    margin:'0 auto',
+  },
+  appbarTitle:{
+    flexGrow: '1',
+    fontSize: '1.9rem'
+  },
+  icon:{
+    color: '#fff',
+    fontSize: '2rem',
+  },
+  colorText:{
+    color:"#7D41FF",
+  },
+  container:{
+    textAlign: 'center',
+  },
+  title:{
+    color: '#fff',
+    fontSize: '3.2rem',
+  },
+  goDown: {
+    color: '#7D41FF',
+    fontSize: '4rem',
+  },
+}));
+export default function Header() {
+  const classes = useStyles();
+  const [checked,setChecked] = useState(false);
+  useEffect(()=>{
+    setChecked(true);
+  },[])
+  return (
+    <div className={classes.root}>
+      <AppBar className={classes.appbar} elevation={0}>
+        <Toolbar className={classes.appbarWrapper}>
+          <h1 className={classes.appbarTitle}>
+            Stock<span className={classes.colorText}>Forum</span>
+          </h1>
+          <IconButton>
+            <SortIcon className={classes.icon} />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+
+      <Collapse in={checked} {...(checked ? { timeout: 1700 } : {})} collapsedHeight={10}>
+        <div className={classes.container}>
+          <h1 className={classes.title}>
+            Welcome to <br /> Stock
+            <span className={classes.colorText}>Forum</span>
+          </h1>
+          <IconButton>
+            <ExpandMoreIcon className={classes.goDown} />
+          </IconButton>
+        </div>
+      </Collapse>
+    </div>
+  );
+}
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
+import Header from '../components/Header';
 import { Link } from 'react-router-dom';
 
+const useStyles = makeStyles((theme) => ({
+  root:{
+    minHeight: '100vh',
+    backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/bg.jpeg'})`,
+    backgroundRepeat:"no-repeat",
+    backgroundSize: 'cover',
+  },
+}));
+
 export default function LandingPage() {
-  return(
-    <>
-      <center>
-        <h2>Landing Page</h2>
-        <Link to='/sign-in'>
-          <button>
-            Sign In
-          </button>
-        </Link>
-      </center>
-    </>
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <Header />
+    </div>
   );
 }
