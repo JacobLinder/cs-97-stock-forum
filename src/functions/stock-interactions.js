de4811e918087d8e5da773aec725b3b9abd47043
JacobLinder <jacob.linder.jl@gmail.com>
2021-02-27 14:14:54 -0600

    Finished stock follows and sentiment

--- src/functions/stock-interactions.js
+++ src/functions/stock-interactions.js
@@ -17,35 +17,9 @@ if (firebase.app.length === 0)
 const db = firebase.firestore();
 
 /**
- * 
- * @param {*} uid 
- * @param {*} ticker 
- * @async
- * @returns 0 if followed, 1 if unfollowed, 2 if error
- */
-export async function toggleFollow(uid, ticker) {
-  try {
-    const followed = await checkFollowStatus(uid, ticker);
-    const userRef = db.collection('users').doc(uid);
-    const followList = (await userRef.get()).data().followed.slice();
-    if (followed) {
-      const index = followList.indexOf(ticker);
-      followList.splice(index, 1);
-    } else {
-      followList.push(ticker);
-    }
-    await userRef.update({followed: followList});
-    return !followed ? 0 : 1;
-  } catch(err) {
-    console.log(err);
-    return 2;
-  }
-}
-
-/**
- * 
- * @param {*} uid 
- * @param {*} ticker 
+ * Checks whether or not a user has followed a stock.
+ * @param {string} uid - the user's uid
+ * @param {string} ticker - the stock ticker
  * @async
  * @returns true if followed, false if not followed
  */
@@ -62,19 +36,40 @@ export async function checkFollowStatus(uid, ticker) {
 }
 
 /**
- * 
- * @param {*} uid 
- * @param {*} ticker 
- * 
+ * Gets a list of all stocks the user followed
+ * @param {string} uid - the user's uid
+ * @async
+ * @returns array of strings of followed stocks, empty array if error
  */
-export async function toggleBullish(uid, ticker) {
+export async function getFollowedStocks(uid) {
   try {
-    const date = (new Date()).toDateString();
-    await db.collection('users').doc(uid).collection('sentiment').doc(ticker).set({
-      sentiment: 'bullish',
-      date: date
-    });
-    await db.collection('')
+    const followList = (await db.collection('users').doc(uid).get()).data().followed.slice();
+    return followList;
+  } catch(err) {
+    console.log(err);
+    return [];
+  }
+}
+
+/**
+ * Handles the backend for the user's click of the follow button
+ * @param {string} uid - the user's uid
+ * @param {string} ticker - the stock ticker
+ * @async
+ * @returns 0 if followed, 1 if unfollowed, 2 if error
+ */
+export async function toggleFollow(uid, ticker) {
+  try {
+    const followed = await checkFollowStatus(uid, ticker);
+    const followList = await getFollowedStocks(uid);
+    if (followed) {
+      const index = followList.indexOf(ticker);
+      followList.splice(index, 1);
+    } else {
+      followList.push(ticker);
+    }
+    await db.collection('users').doc(uid).update({followed: followList});
+    return !followed ? 0 : 1;
   } catch(err) {
     console.log(err);
     return 2;
@@ -82,17 +77,17 @@ export async function toggleBullish(uid, ticker) {
 }
 
 /**
- * 
- * @param {*} uid 
- * @param {*} ticker 
+ * Checks the users daily sentiment for a particular stock
+ * @param {string} uid - the user's uid
+ * @param {string} ticker - the stock ticker
  * @async
  * @returns 0 if none, 1 if bullish, 2 if neutral, 3 if bearish, 4 if error
  */
-export async function checkStatus(uid, ticker) {
+export async function checkUserSentiment(uid, ticker) {
   try {
     const date = (new Date()).toDateString();
     const doc = await db.collection('users').doc(uid).collection('sentiment').doc(ticker).get();
-    if (!doc.exists || doc.data().date !== date)
+    if (!doc.exists || doc.data().sentiment === 'none')
       return 0;
     else if (doc.data().sentiment === 'bullish')
       return 1;
@@ -108,8 +103,134 @@ export async function checkStatus(uid, ticker) {
   }
 }
 
-export async function getStockSentiment(ticker, date) {
-  const doc = await db.collection('stocks').doc(ticker).collection('sentiment').doc(date).get();
-  if (!doc.exists)
-    return {};
+/**
+ * Handles the toggle for each of the sentiment buttons.
+ * @param {string} uid - the user's uid
+ * @param {string} ticker - the stock ticker
+ * @param {string} sentiment - either bullish, neutral, or bearish
+ * @async
+ * @returns 0 if none, 1 if bullish, 2 if neutral, 3 if bearish, 4 if error
+ */
+async function updateUserSentiment(uid, ticker, sentiment) {
+  try {
+    const oldSentiment = await checkUserSentiment(uid, ticker);
+    const newSentiment = sentiment === oldSentiment ? 'none' : sentiment;
+    const date = (new Date()).toDateString();
+    await db.collection('users').doc(uid).collection('sentiment').doc(ticker).set({
+      sentiment: newSentiment,
+      date: date
+    });
+    const stockDoc = await db.collection('stocks').doc(ticker).get();
+    if (stockDoc.exists) {
+      const data = stockDoc.data();
+      if (oldSentiment === 'bullish')
+        data.bullish -= 1;
+      else if (oldSentiment === 'neutral')
+        data.neutral -= 1;
+      else if (oldSentiment === 'bearish')
+        data.bearish -= 1;
+      if (newSentiment === 'bullish')
+        data.bullish += 1;
+      else if (newSentiment === 'neutral')
+        data.neutral += 1;
+      else if (newSentiment === 'bearish')
+        data.bearish += 1;
+      await stockDoc.ref.update({
+        bullish: data.bullish,
+        neutral: data.neutral,
+        bearish: data.bearish
+      });
+    } else {
+      await stockDoc.set({
+        bullish: newSentiment === 'bullish' ? 1 : 0,
+        neutral: newSentiment === 'neutral' ? 1 : 0,
+        bearish: newSentiment === 'bearish' ? 1 : 0
+      });
+    }
+    const returnInt = newSentiment === 'none' ? 0
+      : newSentiment === 'bullish' ? 1
+      : newSentiment === 'neutral' ? 2
+      : newSentiment === 'bearish' ? 3 : 4;
+    return returnInt;
+  } catch(err) {
+    console.log(err);
+    return 4;
+  }
+}
+
+/**
+ * Handles what happens when the user toggles the bullish button.
+ * @param {string} uid - the user's uid
+ * @param {string} ticker - the stock ticker
+ * @async
+ * @returns 0 if none, 1 if bullish, 2 if neutral, 3 if bearish, 4 if error
+ */
+export async function toggleBullish(uid, ticker) {
+  try {
+    const sentimentStatus = await updateUserSentiment(uid, ticker, 'bullish');
+    return sentimentStatus;
+  } catch(err) {
+    console.log(err);
+    return 4;
+  }
+}
+
+/**
+ * Handles what happens when the user toggles the neutral button.
+ * @param {string} uid - the user's uid
+ * @param {string} ticker - the stock ticker
+ * @async
+ * @returns 0 if none, 1 if bullish, 2 if neutral, 3 if bearish, 4 if error
+ */
+export async function toggleNeutral(uid, ticker) {
+  try {
+    const sentimentStatus = await updateUserSentiment(uid, ticker, 'neutral');
+    return sentimentStatus;
+  } catch(err) {
+    console.log(err);
+    return 4;
+  }
+}
+
+/**
+ * Handles what happens when the user toggles the bearish button.
+ * @param {string} uid - the user's uid
+ * @param {string} ticker - the stock ticker
+ * @async
+ * @returns 0 if none, 1 if bullish, 2 if neutral, 3 if bearish, 4 if error
+ */
+export async function toggleBearish(uid, ticker) {
+  try {
+    const sentimentStatus = await updateUserSentiment(uid, ticker, 'bearish');
+    return sentimentStatus;
+  } catch(err) {
+    console.log(err);
+    return 4;
+  }
+}
+
+/**
+ * Gets the total sentiment for a stock
+ * @param {string} ticker - the stock ticker
+ * @async
+ * @returns - {bullish: int, neutral: int, bearish: int} sentiment object
+ */
+export async function getSentimentHistory(ticker) {
+  var stockSentiment = {
+    bullish: 0,
+    neutral: 0,
+    bearish: 0
+  };
+  try {
+    const doc = await db.collection('stocks').doc(ticker).get();
+    if (doc.exists) {
+      stockSentiment.bullish = doc.data().bullish;
+      stockSentiment.neutral = doc.data().neutral;
+      stockSentiment.bearish = doc.data().bearish;
+    }
+    return stockSentiment;
+  } catch(err) {
+    console.log(err);
+    return stockSentiment;
+  }
 }
