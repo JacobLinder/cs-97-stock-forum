f92a180e7fb3d2bac2550b18f36c79db1108bc0c
rahulmadnawat <59585630+rahulmadnawat@users.noreply.github.com>
2021-02-28 18:05:05 -0800

    Create MiniGraph.js

---
+++ src/MiniGraph.js
@@ -0,0 +1,23 @@
+import { csv } from 'd3';
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import { VictoryLine, VictoryChart } from 'victory';
+
+const CSV = 'SPY.csv';
+
+const Graph = () => {
+  const [price, setPrice] = useState([]);
+  
+  useEffect(() => { 
+    csv(CSV, (ticker) => { return +ticker.Price; } )
+      .then(setPrice); }, []);
+  
+  return (
+    <VictoryChart width='600' height='300'>
+    <VictoryLine data={price} x='Date' y='Price' />
+    </VictoryChart>
+  );
+};
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<Graph />, rootElement);
