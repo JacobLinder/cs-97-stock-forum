c3cc77595c77eeafd6af7a373d8464de4db9da1b
frsqy <79062849+ryanschen@users.noreply.github.com>
2021-03-08 01:52:53 -0800

    basic comment functionality
    Added basic commenting in list style to comment page

--- src/pages/CommentPage.js
+++ src/pages/CommentPage.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+function Comment({text, ticker, timestamp}){
+  return(
+    <>
+      <div>{timestamp}</div>
+      <div>
+        {ticker}: {text}
+      </div>
+    </>
+  );
+}
+
 export default function CommentPage() {
+  const data = [ { text: 'I like the stock', ticker: 'AAPL', timestamp: '02:02:02' }, { text: 'I hate the stock', ticker: 'TSLA', timestamp: '01:01:01'}]
+  const listItems = data.map((d) => <li key={d.text}>{d.timestamp} <br/>{d.ticker}: {d.text}</li>);
+
   return(
     <>
       <center>
-        <h2>Comment Page</h2>
+        <h2>User's Comment Page</h2>
         <Link to='/stock-page'>
           <button>
             Stock Page
           </button>
         </Link>
+        <div>{listItems}</div>
       </center>
     </>
   );
