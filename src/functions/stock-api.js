7bf64e823267df8af6378420a2d4c272a11996b1
JacobLinder <jacob.linder.jl@gmail.com>
2021-03-08 23:41:59 -0800

    adding stock sorting

---
+++ src/functions/stock-api.js
@@ -0,0 +1,41 @@
+import tickers from './tickers';
+
+const stockList = [];
+
+export async function initializeStocks() {
+  tickers.forEach(ticker => {
+    // await 
+  });
+}
+
+export function getAllStocks() {
+  return stockList;
+}
+
+export function getFollowedStocks() {
+
+}
+
+export function getUnfollowedStocks() {
+
+}
+
+export function sortMarketCapHigh() {
+
+}
+
+export function sortMarketCapLow() {
+
+}
+
+export function sortPercentIncrease() {
+
+}
+
+export function sortPercentDecrease() {
+
+}
+
+export function searchStocks(text) {
+
+}
