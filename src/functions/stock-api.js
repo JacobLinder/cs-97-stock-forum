a92e32f51257ea0d0f35f4b171338aa4ad620065
JacobLinder <jacob.linder.jl@gmail.com>
2021-03-10 15:55:34 -0800

    updating stocks

--- src/functions/stock-api.js
+++ src/functions/stock-api.js
@@ -1,4 +1,5 @@
 import tickers from './tickers';
+import { getFollowedStocks } from './stock-interactions.js';
 
 const stockList = [];
 
@@ -12,12 +13,26 @@ export function getAllStocks() {
   return stockList;
 }
 
-export function getFollowedStocks() {
-
+export async function getFollowedStocks(uid) {
+  const followed = await getFollowedStocks(uid);
+  const followedList = [];
+  stockList.forEach((stock) => {
+    if (followed.includes(stock.name)) {
+      followedList.push(stock);
+    }
+  });
+  return followedList;
 }
 
-export function getUnfollowedStocks() {
-
+export async function getUnfollowedStocks(uid) {
+  const followed = await getFollowedStocks(uid);
+  const unfollowedList = [];
+  stockList.forEach((stock) => {
+    if (!followed.includes(stock.name)) {
+      unfollowedList.push(stock);
+    }
+  });
+  return unfollowedList;
 }
 
 export function sortMarketCapHigh() {
@@ -37,5 +52,10 @@ export function sortPercentDecrease() {
 }
 
 export function searchStocks(text) {
-
+  const searchedList = [];
+  stockList.forEach((stock) => {
+    if (stock.name.substring(0, text.length) === text) {
+      searchedList.push(stock);
+    }
+  });
 }
