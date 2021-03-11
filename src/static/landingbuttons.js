a0a0c2066bd882831073d77da3d99bfbd76dec47
Nukidy <nuknuk@ucla.edu>
2021-03-11 01:53:11 -0800

    Changed preview stocks to create account

--- src/static/landingbuttons.js
+++ src/static/landingbuttons.js
@@ -3,17 +3,17 @@ const landingbuttons = [
     title: 'Sign in',
     description:
       "Sign in to view stocks! You can start following and interacting with stocks.",
-    imageUrl: process.env.PUBLIC_URL + '/assets/ipad.jpeg',
+    imageUrl: process.env.PUBLIC_URL + '/assets/previewstocks.jpeg',
     time: 1500,
     link: '/sign-in'
   },
   {
-    title: 'Preview stocks',
+    title: 'Create account',
     description:
-      "Don't want to sign in? Click here to view some featured stocks.",
-    imageUrl: process.env.PUBLIC_URL + '/assets/previewstocks.jpeg',
+      "Are you new here? Click here to create an account and start viewing stocks.",
+    imageUrl: process.env.PUBLIC_URL + '/assets/ipad.jpeg',
     time: 1500,
-    link: '/homepage'
+    link: '/create-account'
   },
 ];
 
