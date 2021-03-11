24fdd554e677ac62a1186cebac2626018256aa7b
Nukidy <nuknuk@ucla.edu>
2021-03-11 06:51:47 -0800

    base layout update

--- src/components/navbar/actions/Actions.js
+++ src/components/navbar/actions/Actions.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import "./Actions.css";
-import Button from '../button/Button';
+import Button from '../../button/Button';
 
 export default function Actions() {
   return (
