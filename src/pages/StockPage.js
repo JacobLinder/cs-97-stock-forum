af0eefe866682e3c024b8b93c4980dc9d6870b60
JacobLinder <jacob.linder.jl@gmail.com>
2021-03-11 18:38:01 -0800

    stock page title

--- src/pages/StockPage.js
+++ src/pages/StockPage.js
@@ -1,28 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Paper } from '@material-ui/core'
 import Chat from '../components/StockPageChat/StockPageChat'
+import firebase from 'firebase/app';
 
-export default function StockPage() {
+export default function StockPage(props) {
 
-  const user = "user";
-  const stock = "ticker";
+  const [user, setUser] = useState(null);
+  const stock = props.location.search.replace('?', '');
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((firebaseUser) => {
+      setUser(firebaseUser);
+    })
+  }, []);
+
   return(
     <>
       <center>
-        <h2>{user}'s Stock Page</h2>
+        <h2>{stock} Stock Page</h2>
         <Link to='/landing-page'>
           <button>
             Landing Page
           </button>
         </Link>
       </center>
-      <Paper style={{margin: 50}}>
+      {user ? 
+        <Paper style={{margin: 50}}>
           <Chat
             user={ user }
             stock={ stock }
           />
-      </Paper>
+        </Paper>
+      : null}
     </>
   );
 }
