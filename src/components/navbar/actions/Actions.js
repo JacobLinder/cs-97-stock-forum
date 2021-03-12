d1d20a6325c057ac9f956cac91df7457974c9e87
frsqy <79062849+ryanschen@users.noreply.github.com>
2021-03-11 20:28:58 -0800

    Username can now be updated and link properly

--- src/components/navbar/actions/Actions.js
+++ src/components/navbar/actions/Actions.js
@@ -4,9 +4,11 @@ import "./Actions.css";
 import Button from '../../button/Button';
 
 export default function Actions() {
+
+  const username = "Username";
   return (
     <div className="actions">
-      <Button label="Hi, Username!"/>
+      <Button label={"Hi, "+username+"!"}/>
       <Link to='../pages/landing'>
         <Button label="Sign Out"/>
       </Link>
