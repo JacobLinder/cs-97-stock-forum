1ca9c2ab8bdee57c99ed2b99792b6f413757e7a4
frsqy <79062849+ryanschen@users.noreply.github.com>
2021-03-11 20:21:09 -0800

    cleaned up navbar

--- src/components/navbar/actions/Actions.js
+++ src/components/navbar/actions/Actions.js
@@ -6,8 +6,8 @@ import Button from '../../button/Button';
 export default function Actions() {
   return (
     <div className="actions">
+      <Button label="Hi, Username!"/>
       <Link to='../pages/landing'>
-        <Button label="Hi, Username!"/>
         <Button label="Sign Out"/>
       </Link>
     </div>
